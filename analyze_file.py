bb849ef9045837fe0efe75867bddd37109615426
Konstantinos Niktas <kniktas@palantir.com>
2015-07-17 14:35:19 -0700

    Split out actual repo parsing logic

--- analyze_file.py
+++ analyze_file.py
@@ -1,100 +1,33 @@
 
 import gitraw
-import sys
-import os
-import json
 import glob
-import re
-import nodes
 import graphviz
+import json
+import nodes
+import os
+import parserepolog
+import re
+import sys
 
-ZERO = "0000000"
-DESTRUCTION = "%s-DESTRUCTION" % ZERO
-CREATION = "%s-CREATION" % ZERO
+DESTRUCTION = "%s-DESTRUCTION" % parserepolog.ZERO
+CREATION = "%s-CREATION" % parserepolog.ZERO
 
 source = sys.argv[1]
-globs = glob.glob(os.path.join(source,'*.repo'))
-# just regex it for id
 
-repos = {}
-for currentrepo in globs:
-    m = re.search('.*' + os.path.sep + '([0-9]+)\.repo', currentrepo)
-    if m is None:
-        continue
-    repo_id = m.group(1)
-    with open(os.path.join(source,"%s.repo" % repo_id)) as f:
-        repo_url = "".join(f.readlines()).strip()
-    with open(os.path.join(source,"%s.HEAD" % repo_id)) as f:
-        repo_HEAD = "".join(f.readlines()).strip()
-    with open(os.path.join(source,"%s.log" % repo_id)) as f:
-        repo_info = f.readlines()
-    if repo_HEAD.startswith("ref: "):
-        repo_HEAD = repo_HEAD[len("ref: "):]
-    if len(repo_info) > 0:
-        repos[repo_id] = gitraw.Repo(repo_id, repo_url, repo_HEAD)
-        repos[repo_id].fulllog = repo_info
-
-#print "%d repos with data." % len(repos)
-
-for repo_id in repos:
-    repo = repos[repo_id]
-    commits = []
-    commit = None
-    for line in repo.fulllog:
-        if line.startswith("commit"):
-            if commit is not None:
-                commits.append(commit)
-            commit = gitraw.State(line[len("commit "):].strip())
-            commit.repo = repo
-        elif line.startswith("Author: "):
-            commit.author = line[len("Author: "):].strip()
-        elif line.startswith("Date:"):
-            commit.date = line[len("Date:"):].strip()
-        elif line.startswith("Merge:"):
-            commit.merge = line[len("Merge:"):].strip()
-        elif line.startswith("diff"):
-            commit.diff = []
-            commit.diff.append(line)
-        elif commit.diff is not None:
-            commit.diff.append(line)
-        elif line.startswith(":"):
-            m = re.search(":(\d+)\s+(\d+)\s+([a-f0-9]+)(\.{3})?\s+([a-f0-9]+)(\.{3})?\s+[A-Z]\s+(.*)", line)
-            if m is not None:
-                if commit.blob_before is not None and commit.blob_after is not None:
-                    # Try to deal with rename case
-                    if commit.blob_before == ZERO:
-                        commit.mode_before = m.group(1).strip()
-                        commit.blob_before = m.group(3).strip()
-                        commit.path = m.group(7).strip()
-                    if commit.blob_after == ZERO:
-                        commit.mode_after = m.group(2).strip()
-                        commit.blob_after = m.group(5).strip()
-                else:
-                    commit.mode_before = m.group(1).strip()
-                    commit.mode_after = m.group(2).strip()
-                    commit.blob_before = m.group(3).strip()
-                    commit.blob_after = m.group(5).strip()
-                    commit.path = m.group(7).strip()
-            else:
-                print "m is None for %s" % line
-    if commit is not None:
-        commits.append(commit)
-    repo.commits = commits
-    repo.fulllog = None
+repos = parserepolog.RepoLogParse(source).parse_repo()
 
 node_store = {}
 node_store[CREATION] = nodes.Node(CREATION)
 node_store[DESTRUCTION] = nodes.Node(DESTRUCTION)
 for repo_id in repos:
     repo = repos[repo_id]
-    #previous_node = node_store[CREATION]
     previous_node = None
     for commit in repo.commits:
         if commit.merge is not None:
             continue
-        if commit.blob_before == ZERO:
+        if commit.blob_before == parserepolog.ZERO:
             previous_node = node_store[CREATION]
-        if commit.blob_after == ZERO:
+        if commit.blob_after == parserepolog.ZERO:
             current_node = node_store[DESTRUCTION]
         else:
             if commit.blob_after not in node_store:
---
+++ parserepolog.py
@@ -0,0 +1,82 @@
+
+import gitraw
+import glob
+import os
+import re
+
+ZERO = "0000000"
+
+class RepoLogParse:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def parse_repo(self):
+        globs = glob.glob(os.path.join(self.directory,'*.repo'))
+
+        # First parse the repo/HEAD files and store the log file
+        repos = {}
+        for currentrepo in globs:
+            m = re.search('.*' + os.path.sep + '([0-9]+)\.repo', currentrepo)
+            if m is None:
+                continue
+            repo_id = m.group(1)
+            with open(os.path.join(self.directory,"%s.repo" % repo_id)) as f:
+                repo_url = "".join(f.readlines()).strip()
+            with open(os.path.join(self.directory,"%s.HEAD" % repo_id)) as f:
+                repo_HEAD = "".join(f.readlines()).strip()
+            with open(os.path.join(self.directory,"%s.log" % repo_id)) as f:
+                repo_info = f.readlines()
+            if repo_HEAD.startswith("ref: "):
+                repo_HEAD = repo_HEAD[len("ref: "):]
+            if len(repo_info) > 0:
+                repos[repo_id] = gitraw.Repo(repo_id, repo_url, repo_HEAD)
+                repos[repo_id].fulllog = repo_info
+
+        # Parse the actual log contents, delete the pure contents
+        for repo_id in repos:
+            repo = repos[repo_id]
+            commits = []
+            commit = None
+            for line in repo.fulllog:
+                if line.startswith("commit"):
+                    if commit is not None:
+                        commits.append(commit)
+                    commit = gitraw.State(line[len("commit "):].strip())
+                    commit.repo = repo
+                elif line.startswith("Author: "):
+                    commit.author = line[len("Author: "):].strip()
+                elif line.startswith("Date:"):
+                    commit.date = line[len("Date:"):].strip()
+                elif line.startswith("Merge:"):
+                    commit.merge = line[len("Merge:"):].strip()
+                elif line.startswith("diff"):
+                    commit.diff = []
+                    commit.diff.append(line)
+                elif commit.diff is not None:
+                    commit.diff.append(line)
+                elif line.startswith(":"):
+                    m = re.search(":(\d+)\s+(\d+)\s+([a-f0-9]+)(\.{3})?\s+([a-f0-9]+)(\.{3})?\s+[A-Z]\s+(.*)", line)
+                    if m is not None:
+                        if commit.blob_before is not None and commit.blob_after is not None:
+                            # Try to deal with rename case
+                            if commit.blob_before == ZERO:
+                                commit.mode_before = m.group(1).strip()
+                                commit.blob_before = m.group(3).strip()
+                                commit.path = m.group(7).strip()
+                            if commit.blob_after == ZERO:
+                                commit.mode_after = m.group(2).strip()
+                                commit.blob_after = m.group(5).strip()
+                        else:
+                            commit.mode_before = m.group(1).strip()
+                            commit.mode_after = m.group(2).strip()
+                            commit.blob_before = m.group(3).strip()
+                            commit.blob_after = m.group(5).strip()
+                            commit.path = m.group(7).strip()
+                    else:
+                        print "m is None for %s" % line
+            if commit is not None:
+                commits.append(commit)
+            repo.commits = commits
+            repo.fulllog = None
+
+        return repos
